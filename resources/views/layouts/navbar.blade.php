bfbce95b1b0e3ea70321b9d1464e5ee6163bd479
Dylan Hudson <dylanhudson1998@gmail.com>
2020-01-05 19:54:29 +0000

    Material Design navbar implemented and DB

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,23 @@
+<div class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="javascript:void(0)">Brand</a>
+        </div>
+        <div class="navbar-collapse collapse navbar-responsive-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="javascript:void(0)">Home</a></li>
+                <li><a href="javascript:void(0)">Add New</a></li>
+            </ul>
+            <form class="navbar-form navbar-left">
+                <div class="form-group">
+                    <input type="text" class="form-control col-md-8" placeholder="Search">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
