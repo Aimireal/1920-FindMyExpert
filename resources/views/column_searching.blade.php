ed9ef3c587c736b88f67421f2f14f625a9a73b45
Dylan Hudson <dylanhudson1998@gmail.com>
2020-01-09 12:23:59 +0000

    Navbar display fixed on browse screen

--- resources/views/column_searching.blade.php
+++ resources/views/column_searching.blade.php
@@ -5,71 +5,79 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" />
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" />
 
+@extends('layouts.app')
+@section('content')
+    @if(session('successMsg'))
+        <div class="alert alert-dismissible alert-success">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+            <strong>Well done!</strong> {{session('successMsg')}}
+        </div>
+    @endif
 
-<div class="container">
-    <br />
-    <div class="table-responsive">
-        <table class="table table-bordered table-striped" id="expert_table">
-            <thead>
-            <tr>
-                <th>Business Name</th>
-                <th>
-                    <select name="category_filter" id="category_filter" class="form-control">
-                        <option value="">Select Business Type</option>
-                        @foreach($category as $row)
-                            <option value="{{$row->category_id}}">{{$row->category_name}}</option>
-                        @endforeach
-                    </select>
-                </th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-            </tr>
-            </thead>
-        </table>
+    <div class="container">
+        <br />
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped" id="expert_table">
+                <thead>
+                <tr>
+                    <th>Business Name</th>
+                    <th>
+                        <select name="category_filter" id="category_filter" class="form-control">
+                            <option value="">Select Business Type</option>
+                            @foreach($category as $row)
+                                <option value="{{$row->category_id}}">{{$row->category_name}}</option>
+                            @endforeach
+                        </select>
+                    </th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                </tr>
+                </thead>
+            </table>
+        </div>
     </div>
-</div>
-</body>
 
-<script>
-    $(document).ready(function(){
-        fetch_data();
-        function fetch_data(category = '')
-        {
-            $('#expert_table').DataTable({
-                processing:true,
-                serverSide:true,
-                ajax:{
-                    url:"{{route('column-searching.index')}}",
-                    data: {category:category}
-                },
-                columns:[
-                    {
-                        data: 'company_name', name: 'company_name'
+    <script>
+        $(document).ready(function(){
+            fetch_data();
+            function fetch_data(category = '')
+            {
+                $('#expert_table').DataTable({
+                    processing:true,
+                    serverSide:true,
+                    ajax:{
+                        url:"{{route('column-searching.index')}}",
+                        data: {category:category}
                     },
-                    {
-                        data: 'category_name', name: 'category_name', orderable: false
-                    },
-                    {
-                        data: 'first_name', name: 'first_name'
-                    },
-                    {
-                        data: 'last_name', name: 'last_name'
-                    },
-                    {
-                        data: 'email', name: 'email'
-                    },
-                    {
-                        data: 'phone', name: 'phone'
-                    }
-                ]
+                    columns:[
+                        {
+                            data: 'company_name', name: 'company_name'
+                        },
+                        {
+                            data: 'category_name', name: 'category_name', orderable: false
+                        },
+                        {
+                            data: 'first_name', name: 'first_name'
+                        },
+                        {
+                            data: 'last_name', name: 'last_name'
+                        },
+                        {
+                            data: 'email', name: 'email'
+                        },
+                        {
+                            data: 'phone', name: 'phone'
+                        }
+                    ]
+                });
+            }
+            $('#category_filter').change(function(){
+                var category_id = $('#category_filter').val();
+                $('#expert_table').DataTable().destroy();
+                fetch_data(category_id);
             });
-        }
-        $('#category_filter').change(function(){
-            var category_id = $('#category_filter').val();
-            $('#expert_table').DataTable().destroy();
-            fetch_data(category_id);
         });
-    });
-</script>
+    </script>
+@endsection
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="{{route('home')}}"> <i class="fa fa-address-book" aria-hidden="true"></i> FindMyExpert</a>
+    <a class="navbar-brand" href="{{route('view')}}"> <i class="fa fa-address-book" aria-hidden="true"></i> FindMyExpert</a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="{{route('view')}}">Home</a>
+                <a class="nav-link" href="{{route('home')}}">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="{{route('column-searching')}}">Browse Experts</a>
